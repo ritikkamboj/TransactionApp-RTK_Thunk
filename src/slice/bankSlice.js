46126045c273fb84669880ae7a241f1c17fd855b
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-18 13:32:08 +0530

    created the bankSlice with reducers and extraReducers

--- src/slice/bankSlice.js
+++ src/slice/bankSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 
 // Below is Async thunk action to add transaction on server/backend 
@@ -21,5 +21,52 @@ export const fetchTransaction = createAsyncThunk('fetchTransaction', async () =>
     return res.json();
 })
 
+// creating the slice 
 
+const bankSlice = createSlice({
+    name: "bank",
+    initialState: {
+        balance: 1000,
+        transactions: [],
+        isError: null,
+        isLoading: false
+    },
+    reducers: {
+        deposit: (state, action) => {
+            state.balance += action.payload
+
+        },
+        withdrawn: (state, action) => {
+            state.balance -= action.payload;
+
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(transactionSave.fulfilled, (state, action) => {
+            state.isLoading = false, state.transactions.push(action.payload)
+        })
+        builder.addCase(transactionSave.pending, (state, action) => {
+            state.isLoading = true, state.isError = null
+        })
+        builder.addCase(transactionSave.rejected, (state, action) => {
+            state.isLoading = false, state.isError = "There is some Error in fetching the data !"
+
+        })
+        builder.addCase(fetchTransaction.pending, (state, action) => {
+            state.isLoading = true, state.isError = null
+        })
+        builder.addCase(fetchTransaction.rejected, (state, action) => {
+            state.isError = "There is some Error in fetching the data !", state.isLoading = false
+        })
+        builder.addCase(fetchTransaction.fulfilled, (state, action) => {
+            state.isLoading = false, state.transactions = action.payload
+        })
+
+    }
+})
+
+
+export default bankSlice.reducers;
+
+export const { withdrawn, deposit } = bankSlice.actions;
 
